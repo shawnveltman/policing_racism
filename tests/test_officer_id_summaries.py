4a7e974b24d58536dc3dd315ee734f712e8b303c
Shawn Veltman <shawn.veltman@gmail.com>
2018-10-12 21:21:34 -0600

    POL-45 added test for police id summary

--- tests/test_officer_id_summaries.py
+++ tests/test_officer_id_summaries.py
@@ -4,4 +4,10 @@ from src.officerid import OfficerId
 def test_can_groupby_officer_id(acs):
     stop = OfficerId('data/stop_data/stops_test.csv', acs=acs)
     stop.create_summary()
-    hello = 'world'
+    summary = stop.summary
+    wy199 = summary['state_officer_id'] == 'wy199'
+    first_fips = summary['county_fips'] == '56001'
+    filtered_dataframe = summary[wy199 & first_fips].iloc[0]
+
+    white_stop_percentage_ = filtered_dataframe['white_stop_percentage']
+    assert white_stop_percentage_ == 4 / 6
